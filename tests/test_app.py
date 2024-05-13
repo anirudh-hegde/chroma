155d25753244ca76f72be58d83c9d1d43b576415
Anirudh Hegde <hegdeanirudh2003@gmail.com>
2024-05-13 18:52:01 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,17 @@
+import os
 import pytest
 from tkinter import Tk, Canvas, Button
 from tkinter.messagebox import showinfo
 from unittest.mock import MagicMock
 from chrom import main_paint, activate_paint, paint, change_color, delete
-import os
 
+if os.environ.get('DISPLAY','') == '':
+        # print('no display found. Using :0.0')
+        os.environ.__setitem__('DISPLAY', ':0.0')
+    
 @pytest.fixture
 def app():
-    if os.environ.get('DISPLAY','') == '':
-        print('no display found. Using :0.0')
-        os.environ.__setitem__('DISPLAY', ':0.0')
+    
     root = Tk()
     cv = Canvas(root, width=640, height=480, bg='white')
     cv.pack()
