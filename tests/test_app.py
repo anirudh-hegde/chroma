7b744433bd2d9c579a96a672d40b5978eec4b69b
Anirudh Hegde <hegdeanirudh2003@gmail.com>
2024-05-13 20:44:47 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,12 @@
 """Tests the GUI app"""
-import os
+import pytest
 from tkinter import Tk, Canvas
 from unittest.mock import MagicMock
-import pytest
 from chrom import main_paint, activate_paint, paint, change_color, delete
-    
+
 @pytest.fixture
 def app():
-    """tests the app"""
+    """Set up the tkinter app for testing"""
     root = Tk()
     cv = Canvas(root, width=640, height=480, bg='white')
     cv.pack()
@@ -15,10 +14,9 @@ def app():
     yield root
     root.destroy()
 
-
 def test_activate_paint():
-    """tests activation of paint"""
-    root=Tk()
+    """Test activation of paint"""
+    root = Tk()
     cv = Canvas(root, width=640, height=480, bg='white')
     event = MagicMock()
     event.x, event.y = 50, 50
@@ -26,21 +24,21 @@ def test_activate_paint():
     assert cv.bind.called_with('<B1-Motion>', paint)
 
 def test_change_color():
-    """tests change in color"""
-    global current_color, initial_color
+    """Test change in color"""
+    global current_color
     initial_color = current_color
     change_color('blue')
     assert current_color == 'blue'
 
 def test_delete():
-    """tests the delete all"""
-    root=Tk()
+    """Test the delete all"""
+    root = Tk()
     cv = Canvas(root, width=640, height=480, bg='white')
     delete()
     assert cv.delete("all")
 
-def test_color_buttons():
-    """tests the color button"""
+def test_color_buttons(app):
+    """Test the color button"""
     global current_color
     color_buttons = app.children['!frame'].winfo_children()
     for btn in color_buttons:
